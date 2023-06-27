c3d47c305b8e0bc2c87ad0aaf0d0b89a1343a292
Jiri Antos <worvanoso@gmail.com>
2023-06-27 12:27:42 +0200

    progress

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeTableComponent } from "./employee-table/employee-table.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/employees', pathMatch: "full"},
+  { path: 'employees', component: EmployeeTableComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/model/empleyee.model.ts
@@ -0,0 +1,7 @@
+export enum GENDER { "Male", "Female" }
+export interface Employee {
+  name: string;
+  jobTitle: string;
+  tenure: string;
+  gender: GENDER;
+}
